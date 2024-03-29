1214383e16d75902676e5f5564c4e1fd8a4b39b7
Sailesh O <sailesh_09@ymail.com>
2024-03-29 08:49:09 -0500

    Update and rename convert.py to convert_yaml_to_hcl.py

---
+++ convert_yaml_to_hcl.py
@@ -0,0 +1,19 @@
+import yaml
+import hclgen
+def yaml_to_hcl(yaml_data):
+    hcl_data = {}
+    for key, value in yaml_data.items():
+        if isinstance(value, dict):
+            hcl_data[key] = yaml_to_hcl(value)
+        else:
+            hcl_data[key] = value
+    return hcl_data
+def main():
+    with open('input.yaml', 'r') as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+    
+    hcl_data = yaml_to_hcl(yaml_data)
+    with open('terraform.tfvars', 'w') as hcl_file:
+        hcl_file.write(hclgen.generate(hcl_data))
+if name == "__main__":
+    main()
